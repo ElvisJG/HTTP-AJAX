1de433e95956d368fbe0f6ddddef515aa88b894f
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-25 19:21:13 -0400

    much boilerplate wow

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
