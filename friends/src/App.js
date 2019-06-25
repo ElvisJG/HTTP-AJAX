edd0ac80d991134209a7033e4241d8e935c19f8d
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-25 19:56:24 -0400

    progressing on friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+import Friends from './components/FriendsCard';
+import Meme from './spongebob.png';
 
 class App extends React.Component {
+  state = {
+    friends: []
+  };
+
+  componentDidMount() {
+    Axios.get('https://localhost:5000/friends').then(res => {
+      this.setState({
+        friends: res.data
+      })
+        .then(() => {
+          return Axios.get('http://localhost:5000/friends');
+        })
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log('Error:', err);
+        });
+    });
+  }
+
   render() {
+    const { friends } = this.state;
     return (
       <div className='App'>
         <header className='App-header'>
-          <h1>Hello World</h1>
+          <h1>The Gang's All Here</h1>
+          <div className='friends and form'>
+            {/* Much Friends Very wow 🐶 */}
+            <Friends {...friends} friends={friends} />
+          </div>
+          <img src={Meme} alt='Spongebobs Hand' className='meme' />
         </header>
       </div>
     );
--- friends/src/components/FriendsCard.js
+++ friends/src/components/FriendsCard.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function FriendsCard(props) {
-  return <div />;
-}
+export default props => {
+  console.log(props);
+  return (
+    <div className='friends'>
+      {props.friends.map(friend => (
+        <div className='friend-card-wrapper' key={friend.id}>
+          <div className='friend-text'>
+            <h2 className='friend-name'>{friend.name}</h2>
+            <h4 className='friend-age'>{friend.age}</h4>
+          </div>
+          <h3 className='email'>{friend.email}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
